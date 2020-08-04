b4533285fc1ba57af5a6b8dbaf33f8ead4f9c3b8
MohammedMujtabaAsif <asifmm@aston.ac.uk>
2020-08-04 17:17:19 +0100

    simplified seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -26,6 +26,14 @@ class UsersTableSeeder extends Seeder
             $dob = $faker->date($format = 'Y-m-d', $max = '-18 years');
             $currentDateTime = Carbon::now();
 
+            $gender = $faker->numberBetween($min = 1, $max = 2);
+
+            if($gender==1){
+                $prefGender = 2;
+            }else{
+                $prefGender = 1;
+            }
+
             $user = User::create([
                 'firstNames' => $firstName,
                 'surname' => $surname,
@@ -35,19 +43,17 @@ class UsersTableSeeder extends Seeder
                 'password' => bcrypt('password'), // password
                 'remember_token' => Str::random(10),
                 'phoneNumber' => $faker->phoneNumber,
-                'city_id' => $faker->numberBetween($min = 1, $max = 2),
-                'gender_id' => $faker->numberBetween($min = 1, $max = 1),
+                'city_id' => $faker->numberBetween($min = 1, $max = 3),
+                'gender_id' => $gender,
                 'marital_status_id' => $faker->numberBetween($min = 1, $max = 3),
                 'dob' => $dob,
                 'age' => Carbon::parse($dob)->diff(Carbon::now())->format('%y'),
-                'numOfChildren' => $faker->numberBetween($min = 0, $max = 2),
+                'numOfChildren' => $faker->numberBetween($min = 0, $max = 3),
                 'bio'  => $faker->realText($maxNbChars = 200, $indexSize = 2),
                 
                 'prefMinAge' => $faker->numberBetween($min = 18, $max = 24),
                 'prefMaxAge' => $faker->numberBetween($min = 25, $max = 35),
                 'prefMaxNumOfChildren' => $faker->numberBetween($min = 0, $max = 3),
-
-
 
                 'created_at' => $currentDateTime,
                 'updated_at' => $currentDateTime,
@@ -55,7 +61,7 @@ class UsersTableSeeder extends Seeder
 
                     
             $user->prefCities()->sync($faker->numberBetween($min = 1, $max = 3));
-            $user->prefGenders()->sync($faker->numberBetween($min = 1, $max = 2));
+            $user->prefGenders()->sync($prefGender);
             $user->prefMaritalStatuses()->sync($faker->numberBetween($min = 1, $max = 3));
 
             $user->save();
