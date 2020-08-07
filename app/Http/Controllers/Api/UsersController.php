cc4623b11cdc9989c9f4ea3ea46b39586f586edd
MohammedMujtabaAsif <asifmm@aston.ac.uk>
2020-08-07 01:22:29 +0100

    new methods and comments in UsersController

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -15,82 +15,148 @@ use App\User;
 class UsersController extends Controller
 {
 
+  /**
+    * Get the authenticated User
+    *
+    * @return Response success User
+    */
+  public function index()
+  {    
+    $user = User::select([
+        'id',
+        'firstNames',
+        'surname',
+        'prefName',
+        'email',
+        'phoneNumber',
+        'gender_id',
+        'city_id',
+        'marital_status_id',
+        'dob',
+        'age',
+        'numOfChildren',
+        'bio',
+        'prefMinAge',
+        'prefMaxAge',
+        'prefMaxNumOfChildren',
+      ])
+      ->with('gender')
+      ->with('city')
+      ->with('maritalStatus')
+      ->with('prefCities')
+      ->with('prefGenders')
+      ->with('prefMaritalStatuses')
+      ->where('id', request()->user()->id)->first();
 
-  // /**
-  //  * Create user
-  //  *
-  //  * @param  [string] name
-  //  * @param  [string] email
-  //  * @param  [string] password
-  //  * @param  [string] password_confirmation
-  //  * @return [string] message
-  //  */
-  // public function register(Request $request)
-  //     {
-  //         $validator = Validator::make($request->all(), [
-  //         //Validate user's personal details
-  //         'firstNames' => 'required|string',
-  //         'surname' => 'required|string',
-  //         'prefName'=>'required|string',
-  //         'email' => 'required|string|email|unique:users',
-  //         'password' => 'required|string|confirmed',
-  //         'phoneNumber' => 'required|unique:users|max:11|regex:/(0)[0-9]{10}/',
-  //         'gender'=>'required|int',
-  //         'userCity' => 'required|int',
-  //         'maritalStatus'=>'required|int',
-  //         'dob'=>'required|date|before:18 years ago',
-  //         'numOfChildren'=>'int',
-  //         'bio'=>'required|string|max:1000',
-
-  //         //Validate user's partner preferences
-  //         // 'prefCities' => 'required|int|array|distinct',
-  //         // 'prefGenders'=>'required|int|array|distinct',
-  //         // 'prefMaritalStatuses'=>'required|int|array|distinct',
-  //         // 'prefMinAge'=>'required|int|min:18|lt:prefMaxAge',
-  //         // 'prefMaxAge'=>'required|int|min:20|gt:prefMinAge',
-  //         // 'prefMaxNumOfChildren'=>'required|int',
-  //         ]);
-
-  //         if ($validator->fails()) {
-  //             return response()->json([
-  //             'success' => false,
-  //             'message' => $validator->errors(),
-  //             'code' => 400,
-  //             ]);
-  //         }
-
-  //         $input = $request->all();
-  //         $input['password'] = Hash::make($input['password']);            
-  //         $input['age'] = Carbon::parse($input['dob'])->diff(Carbon::now())->format('%y');
-  //         $user = User::create($input);
-
-  //         $token['token'] = $user->createToken('appToken')->accessToken;
-
-  //         // return verificationCheck($user, $token);
-  //         return response()->json([
-  //             'success' => true,
-  //             'token' => $token,
-  //             'user' => $user,
-  //             'code' => 200,
-  //         ]);
-  //     }
-
-  public function verificationCheck(){
+    if(is_null($user))
+      return response()->json([
+        'success' => false,
+        'message' => 'User Not Found',
+      ]);
+    
     return response()->json([
       'success' => true,
-      'message' => 'Authorised',
+      'data' => $user
     ]);
   }
 
 
+  /**
+    * Validate then Update the authenticated user's details to request details
+    *
+    * @param Request $request user's account details (except password)
+    *
+    * @return Response success User
+    */
   public function update(Request $request){
     $user = $request->user();
 
+    $validator = Validator::make($request->all(), [
+      //Validate user's personal details
+      'firstNames' => 'required|string',
+      'surname' => 'required|string',
+      'prefName'=>'required|string',
+      'email' => 'required|string|email|unique:users',
+      'phoneNumber' => 'required|unique:users|max:11|regex:/(0)[0-9]{10}/|string',
+      'city_id' => 'required|integer',
+      'gender_id'=>'required|integer',
+      'marital_status_id'=>'required|integer',
+      'dob'=>'required|date|before:18 years ago',
+      'numOfChildren'=>'require|integer',
+      'bio'=>'required|string|max:1000',
+      // 'image' => 'file|max:5000',
+
+      //Validate user's partner preferences
+      'prefMinAge'=>'required|integer|min:18|lt:prefMaxAge',
+      'prefMaxAge'=>'required|integer|min:20|gt:prefMinAge',
+      'prefMaxNumOfChildren'=>'required|integer',
+
+      'prefCities' => 'required|integer|array|distinct',
+      'prefGenders'=>'required|integer|array|distinct',
+      'prefMaritalStatuses'=>'required|integer|array|distinct',
+      
+      ]);
+
+      if($validator->fails()){
+        return response()->json([
+          'success' => false,
+          'message' => $validator->errors
+        ]);
+      }
+
+      $user->update([
+        // //User's personal details
+        'firstNames' => $validator['firstNames'],
+        'surname' => $validator['surname'],
+        'prefName'=> $validator['prefName'],
+        'email' => $validator['email'],
+        'password' => bcrypt($validator['password']),
+        'phoneNumber' => $validator['phoneNumber'],
+        'city_id' => (int) $validator['city_id'],
+        'gender_id' => (int) $validator['gender_id'],
+        'marital_status_id' => (int) $validator['marital_status_id'], 
+        'dob' => Carbon::createFromFormat('Y-m-d', input['dob']),
+        'age' => Carbon::parse($validator['dob'])->diff(Carbon::now())->format('%y'),
+        'numOfChildren' => $validator['numOfChildren'],
+        'bio' => $validator['bio'],
+        // TODO: imageAddress
+
+        //User's partner preferences
+        'prefMinAge' => (int) $validator['prefMinAge'],
+        'prefMaxAge' => (int) $validator['prefMaxAge'],
+        'prefMaxNumOfChildren' => (int) $validator['prefMaxNumOfChildren'],
+      ]);
+
+
+    if((int) $validator['gender_id'] == 0){ 
+      $prefGenders = 1;
+    }
+    else{
+      $prefGenders = 0;
+    }
+    $user->prefGenders()->sync($prefGenders);
+    
+    $user->prefCities()->sync((int) $validator['prefCities']);
+    $user->prefMaritalStatuses()->sync((int) $validator['prefMaritalStatuses']);
+
+    return response()->json([
+      'success' => true,
+      'data' => $user
+    ]);
   }
 
+
+  /**
+    * Check user knows account password
+    * Delete the authenticated user
+    *
+    * @param Request $request password
+    *
+    * @return Response success message
+    */
   public function delete(Request $request)
   {
-    $user = User::find(Auth::user()->id);
+    $user = request()->user();
     $hasher = app('hash');
     if ($hasher->check($request['password'], $user->password)) {
       $request->user()->token()->revoke();
@@ -102,33 +168,45 @@ class UsersController extends Controller
         ]);
       }
       else {
-          return response()->json([
-            'success' => false,
-            'message' =>'Failed to delete your account!',
-          ]);
-      }
-    }
-    else{        
-      return response()->json([
+        return response()->json([
           'success' => false,
-          'message' =>'Incorrect Password',
-          'passsword' => $request['password']
+          'message' =>'Failed to delete your account!',
         ]);
       }
+    }
+    else {        
+      return response()->json([
+        'success' => false,
+        'message' =>'Incorrect Password',
+      ]);
+    }
+  }
+
+  /**
+    * If the user can access the verified and approved middleware
+    * Return a positive response
+    *
+    * @return Response
+    */
+  public function verificationCheck(){
+    return response()->json([
+      'success' => true,
+      'message' => 'Authorised',
+    ]);
   }
 
 
   /**
     * Show a list of all of the application's users.
     *
-    * @return Response
+    * @return Response success User
     */
   public function getAllOtherUsers()
   {
 
     // $users = auth()->user()->getAllFriendships();
-
     //TODO: Make SELECT query using User's preferences
+
     $users = User::select([
         'id',
         // 'firstNames',
@@ -161,7 +239,7 @@ class UsersController extends Controller
       ->paginate(15);
 
     if(count($users) === 0)
-      return response()->json([#
+      return response()->json([
         'success' => false,
         'message' => 'No Other Users Have Been Approved Yet']);
 
@@ -173,79 +251,24 @@ class UsersController extends Controller
 
 
   /**
-    * Get the authenticated User
+    * Show a list of all of the application's users.
+    *
+    * @param Request $request id non-authed user
     *
     * @return Response
     */
-  public function getCurrentUser(Request $request)
-  {   
-    
-    $user = $request->user();
-
-    if(is_null($user))
-      return response()->json([
-        'success' => false,
-        'message' => 'User Not Found',
-      ]);
-
-    $userCleaned = $user->only([
-        'id',
-        'firstNames',
-        'surname',
-        'prefName',
-        'email',
-        'phoneNumber',
-        'dob',
-        'age',
-        'numOfChildren',
-        'bio',
-        'prefMinAge',
-        'prefMaxAge',
-        'prefMaxNumOfChildren',
-      ]);
-
-    $userDetails = [
-      'id' => $userCleaned['id'],
-      'firstNames' => $userCleaned['firstNames'],
-      'surname' => $userCleaned['surname'],
-      'prefName' => $userCleaned['prefName'],
-      'email' => $userCleaned['email'],
-      'phoneNumber' => $userCleaned['phoneNumber'],
-      'dob' => $userCleaned['dob'],
-      'age' => $userCleaned['age'],
-      'numOfChildren' => $userCleaned['numOfChildren'],
-      'bio' => $userCleaned['bio'],
-      'prefMinAge' => $userCleaned['prefMinAge'],
-      'prefMaxAge' => $userCleaned['prefMaxAge'],
-      'prefMaxNumOfChildren' => $userCleaned['prefMaxNumOfChildren'],
-      'gender' => $user->gender,
-      'city' => $user->city,
-      'marital_status' => $user->maritalStatus,
-      'pref_cities' => $user->prefCities,
-      'pref_genders' => $user->prefGenders,
-      'pref_marital_statuses' => $user->prefMaritalStatuses,
-      ];
-    
-
-    return response()->json([
-      'success' => true,
-      'data' => $userDetails
-      ]);
-  }
-
-
   public function getUserWithID(Request $request){
       $user = User::select([
         'id',
-        'firstNames',
-        'surname',
+        // 'firstNames',
+        // 'surname',
         'prefName',
-        'email',
-        'phoneNumber',
+        // 'email',
+        // 'phoneNumber',
         'gender_id',
         'city_id',
         'marital_status_id',
-        'dob',
+        // 'dob',
         'age',
         'numOfChildren',
         'bio',
@@ -270,25 +293,24 @@ class UsersController extends Controller
       return response()->json([
         'success' => true,
         'data' => $user
-        ]);
+      ]);
   }
 
 
   /**
     * Get users matched with authenticated user
-    * @param Request $request
+    *
     * @return Response
     */
-  public function getMatchedUsers(Request $request){
-    $user = $request->user();
-    $friends = $user->getFriends(10);
+  public function getAcceptedMatches(){
+    $friends = request()->user()->getFriends(10);
     
     if(count($friends) == 0)
     // if no matches are found, return a message 
       return response()->json([
         'success' -> false,
         'message' => 'No Matches',
-        ]);
+      ]);
     else
     // if matches are found return them as JSON
     foreach($friends as $friend){
@@ -302,12 +324,14 @@ class UsersController extends Controller
       return response()->json([
         'success' => true,
         'data' => $friends
-        ]);
+      ]);
   }
 
   /**
     * Send match request to another user
-    * @param Request $request
+    *
+    * @param Request $request id non-authed user
+    *
     * @return Response
     */
   public function sendMatchRequest(Request $request)
@@ -331,7 +355,7 @@ class UsersController extends Controller
     }
 
     elseif($user->hasSentFriendRequestTo($recipient)){
-        return response()->json([
+      return response()->json([
         'success' => false,
         'message' => 'You Have Already Sent a Match Request to ' . $recipient->prefName,
       ]);
@@ -339,10 +363,10 @@ class UsersController extends Controller
 
     elseif($user->hasFriendRequestFrom($recipient)){
       if($user->befriend($recipient)){
-      return response()->json([        
-        'success' => true,
-        'message' => 'You Matched with ' . $recipient->prefName,
-      ]);
+        return response()->json([        
+          'success' => true,
+          'message' => 'You Matched with ' . $recipient->prefName,
+        ]);
       }
     }
 
@@ -363,13 +387,14 @@ class UsersController extends Controller
 
   /**
     * Get match requests from other users
-    * @param Request $request
+    *
+    *
     * @return Response
     */
-  public function getMatchRequests(Request $request){
-    $matchRequests = $request->user()->getFriendRequests($perPage = 10);
+  public function getPendingMatches(){
+    $pendingMatches = request()->user()->getFriendRequests($perPage = 10);
 
-    if(count($matchRequests) === 0){
+    if(count($pendingMatches) === 0){
       return response()->json([
         'success' => false,
         'message' => 'No Matches Requests Found',
@@ -378,7 +403,7 @@ class UsersController extends Controller
     
     $senders = array();
 
-    foreach($matchRequests as $matchRequest){
+    foreach($pendingMatches as $matchRequest){
       $sender = $matchRequest->sender;
       $sender->gender;
       $sender->city;
@@ -392,13 +417,66 @@ class UsersController extends Controller
     return response()->json([
       'success' => true,
       'data' => $senders
-      ]);
+    ]);
   }
 
 
+  public function getDeniedMatches(){
+    $deniedMatches = request()->user()->getDeniedFriendships();
+    
+    if(count($deniedMatches) == 0)
+      return response()->json([
+        'success' => false,
+        'message' => "No Denied Matches Found"
+      ]);
+
+        $senders = array();
+
+    foreach($deniedMatches as $matchRequest){
+      $sender = $matchRequest->sender;
+      $sender->gender;
+      $sender->city;
+      $sender->maritalStatus;
+      $sender->prefCities;
+      $sender->prefGenders;
+      $sender->prefMaritalStatuses;
+      $senders[] = $sender;
+    }
+
+    return response()->json([
+      'success' => true,
+      'data' => $senders
+    ]);
+
+
+    return response()->json([
+      'success' => true,
+      'data' => $deniedMatches
+    ]);
+  }
+
+
+  public function getBlockedMatches(){
+    $blockedUsers = request()->user()->getBlockedFriendships();
+
+    if(count($blockedUsers) == 0)
+      return response()->json([
+        'success' => false,
+        'message' => "No Denied Matches Found"
+      ]);
+
+    return response()->json([
+      'success' => true,
+      'data' => $blockedUsers
+    ]);
+    
+  }
+
   /**
     * Accept a match request from another user
-    * @param Request $request
+    *
+    * @param Request $request id non-authed user
+    *
     * @return Response
     */
   public function acceptMatchRequest(Request $request){
@@ -416,20 +494,22 @@ class UsersController extends Controller
       return response()->json([
         'success' => true,
         'message' => 'Accepted Match Request from ' . $sender->prefName,
-        ]);
+      ]);
     }
     
     return response()->json([
       'success' => false,
       'message' => 'Failed to Accept Match Request from ' . $sender->prefName,
-      ]);
+    ]);
 
   }
 
 
   /**
     * Deny a match request from another user
-    * @param Request $request
+    *
+    * @param Request $request id non-authed user
+    *
     * @return Response
     */
   public function denyMatchRequest(Request $request){
@@ -446,19 +526,21 @@ class UsersController extends Controller
       return response()->json([
         'success' => true,
         'message' => 'Denied Match Request from ' . $sender->prefName,
-        ]);
+      ]);
     }
 
     return response()->json([
       'success' => false,
       'message' => 'Failed to Deny Match Request from ' . $sender->prefName,
-      ]);
+    ]);
   }
 
 
   /**
     * Remove match with another user
-    * @param Request $request
+    *
+    * @param Request $request id non-authed user
+    *
     * @return Response
     */
   public function unmatch(Request $request){
@@ -488,7 +570,7 @@ class UsersController extends Controller
 
     return response()->json([
       'success' => false,
-      'message' => 'You Must Match with ' . $friend->prefName . ' Before Unmatching',
+      'message' => 'You Must Match With ' . $friend->prefName . ' Before Unmatching',
     ]);
     
   }
@@ -496,10 +578,12 @@ class UsersController extends Controller
 
   /**
     * Block user from authenticated user
-    * @param Request $request
+    *
+    * @param Request $request id non-authed user
+    *
     * @return Response
     */
-  public function blockUser(Request $request){
+  public function blockMatch(Request $request){
     $user = $request->user();
     $userToBlock = User::where('id', $request['id'])->first();
 
@@ -526,10 +610,12 @@ class UsersController extends Controller
 
   /**
     * Unblock user from authenticated user
-    * @param Request $request
+    *
+    * @param Request $request id non-authed user
+    *
     * @return Response
     */
-  public function unblockUser(Request $request){
+  public function unblockMatch(Request $request){
     $user = $requst->user();
     $userToUnblock = User::where('id', $request['id'])->first();
 
@@ -546,6 +632,7 @@ class UsersController extends Controller
       ]);
     }
   }
+
 
 
   // public function storeProfileImage(Request $request){
